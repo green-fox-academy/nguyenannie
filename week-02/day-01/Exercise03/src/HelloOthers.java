fd919b0a5b0110463934eddb4763bbabcc5499aa
nguyenannie <tramtramrhinomo@gmail.com>
2017-11-27 14:41:12 +0100

    Exercise 01 - 06

---
+++ week-02/day-01/Exercise03/src/HelloOthers.java
@@ -0,0 +1,9 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Adel!");
+        System.out.println("Hello, Balint!");
+        System.out.println("Hello, Adam!");
+
+    }
+}
---
+++ week-02/day-01/Exercise04/src/IntroduceYourself.java
@@ -0,0 +1,7 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("Annie");
+        System.out.println(22);
+        System.out.println(1.57);
+    }
+}
