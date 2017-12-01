1105f1f3e458a458854e1a2ded52db91f01beb9d
nguyenannie <tramtramrhinomo@gmail.com>
2017-12-01 14:49:28 +0100

    Complete Counter exercise

---
+++ week-03/day-03/Counter/src/Counter.java
@@ -0,0 +1,16 @@
+public class Counter {
+    public static void main(String[] args) {
+        System.out.println(countDown(5));
+
+    }
+
+    public static int countDown(int n) {
+        if (n == 0) {
+            return 0;
+        } else {
+                System.out.println(n);
+                return countDown(n - 1);
+            }
+        }
+    }
+
