144c9b113e82cefd887e7e6bc7bb096beee38c2b
nguyenannie <tramtramrhinomo@gmail.com>
2017-12-01 09:54:42 +0100

    Completed LinePlayQuarter exercise

---
+++ week-03/day-02/LinePlayQuarters/src/LinePlayQuarters.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class LinePlayQuarters {
+
+    public static void mainDraw(Graphics graphics){
+        // divide the canvas into 4 parts
+        // and repeat this pattern in each quarter:
+        // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/line-play/r1.png]
+        int space = 20;
+        for (int i = 0; i <= WIDTH/space/2; i++) {
+            graphics.drawLine(i * space,0,0,WIDTH/2 - i* space );
+            graphics.drawLine(0, WIDTH/2 + i* space,i * space,WIDTH );
+            graphics.drawLine(WIDTH/2 + i * space,0,WIDTH,i* space );
+            graphics.drawLine(WIDTH/2 + i * space,WIDTH,WIDTH, WIDTH - i *space);
+        }
+
+
+    }
+
+
+    //    Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+    static class ImagePanel extends JPanel{
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+
+        }
+    }
+
+    class Point {
+        public int x, y;
+        Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
+
