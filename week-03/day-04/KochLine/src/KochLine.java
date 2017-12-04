f01937b300c2369a38a5bf003b051ab0de94a413
nguyenannie <tramtramrhinomo@gmail.com>
2017-12-04 19:54:57 +0100

    Complete KochLine exercise

--- week-03/day-04/KochLine/src/KochLine.java
+++ week-03/day-04/KochLine/src/KochLine.java
@@ -5,24 +5,50 @@ import java.awt.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 
-public class KochLine {
+public class KochLine{
 
     public static void mainDraw(Graphics g) {
-
-        kochLine(g,WIDTH/2,WIDTH/2,300,Math.PI/3);
+        recursiveKochLines(g,0,300,600,0,4);
+        //kochLine(g,200,200,300,Math.PI/5);
     }
 
-    public static void kochLine(Graphics g, int x, int y, int size, double angle) {
+    public static void recursiveKochLines(Graphics g, int x, int y, int size, double angle, int n) {
+
         int x1 = x + (int)(Math.cos(angle) * size/3);
         int y1 = y + (int)(Math.sin(angle) * size/3);
 
-        int x2 = x +  (int)(Math.cos(angle + Math.PI/3) * size / 3);
-        int y2= y - (int)(Math.sin(angle + Math.PI/3) * size/3);
+        int x2 = x1 + (int)(Math.cos(angle - Math.PI/3) * size / 3);
+        int y2= y1 + (int)(Math.sin(angle - Math.PI/3) * size/3);
 
-        int x3 = x + (int)(Math.cos(angle) * 2 * size/3);
-        int y3 = y + (int)(Math.sin(angle) * 2 * size/3);
+        int x3 = x2 + (int)(Math.cos(angle + Math.PI/3) * size/3);
+        int y3 = y2 + (int)(Math.sin(angle + Math.PI/3) * size/3);
 
-        int x4 = x + (int)(Math.cos(angle)  * size);
+        int x4 = x + (int)(Math.cos(angle) * size);
+        int y4 = y + (int)(Math.sin(angle) * size);
+
+        if(n == 0) {
+            g.drawLine(x,y,x4,y4);
+            return;
+        }
+
+        recursiveKochLines(g,x,y,size/3,angle,n-1);
+        recursiveKochLines(g,x1,y1,size/3,angle - Math.PI/3,n-1);
+        recursiveKochLines(g,x2,y2,size/3,angle +  Math.PI/3,n-1);
+        recursiveKochLines(g,x3,y3,size/3,angle,n-1);
+
+    }
+    public static void kochLine(Graphics g, int x, int y, int size, double angle) {
+
+        int x1 = x + (int)(Math.cos(angle) * size/3);
+        int y1 = y + (int)(Math.sin(angle) * size/3);
+
+        int x2 = x1 + (int)(Math.cos(angle - Math.PI/3) * size / 3);
+        int y2= y1 + (int)(Math.sin(angle - Math.PI/3) * size/3);
+
+        int x3 = x2 + (int)(Math.cos(angle + Math.PI/3) * size/3);
+        int y3 = y2 + (int)(Math.sin(angle + Math.PI/3) * size/3);
+
+        int x4 = x + (int)(Math.cos(angle) * size);
         int y4 = y + (int)(Math.sin(angle) * size);
 
 
@@ -31,6 +57,45 @@ public class KochLine {
         g.drawLine(x2,y2,x3,y3);
         g.drawLine(x3,y3,x4,y4);
     }
+    static int WIDTH = 600;
+    static int HEIGHT = 623;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+    static class ImagePanel extends JPanel{
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+
+        }
+    }
+
+    //All Failed attemps below :)
+     /*
+    public static void koch(int n, double size) {
+        Turtle kochTurtle = new Turtle();
+        if( n == 0) {
+            kochTurtle.goForward(size);
+        }
+        else {
+            koch( n - 1, size);
+            kochTurtle.turn(Math.PI/3);
+            koch(n - 1, size);
+            kochTurtle.turn(-2 * Math.PI/3);
+            koch(n -1, size);
+            kochTurtle.turn(Math.PI/3);
+            koch(n -1,size);
+            }
+        }
+    }
+    */
 
     /*
     public static void recursiveKochLines(Graphics g, int x, int y, int size, int n) {
@@ -85,26 +150,4 @@ public class KochLine {
 
     }
     */
-
-
-
-    static int WIDTH = 600;
-    static int HEIGHT = 623;
-
-    public static void main(String[] args) {
-        JFrame jFrame = new JFrame("Drawing");
-        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
-        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        jFrame.add(new ImagePanel());
-        jFrame.setLocationRelativeTo(null);
-        jFrame.setVisible(true);
-    }
-    static class ImagePanel extends JPanel{
-        @Override
-        protected void paintComponent(Graphics graphics) {
-            super.paintComponent(graphics);
-            mainDraw(graphics);
-
-        }
-    }
 }
--- week-03/day-04/KochLine/src/Turtle.java
+++ week-03/day-04/KochLine/src/Turtle.java
@@ -1,7 +1,33 @@
+//don't use this one
+
+import java.awt.*;
+
 public class Turtle {
-    public double turn(double angle) {
-        double currentangle = 0;
-        currentangle += angle;
-        return currentangle;
+    public Turtle(){
+
+    }
+    Graphics g;
+    private double x, y;     // turtle is at (x, y)
+    private double angle;    // facing this many degrees counterclockwise from the x-axis
+
+    // start at (x0, y0), facing a0 degrees counterclockwise from the x-axis
+    public Turtle(double x0, double y0, double a0) {
+        x = x0;
+        y = y0;
+        angle = a0;
+    }
+
+    // rotate orientation delta degrees counterclockwise
+    public void turn(double delta) {
+        angle += delta;
+    }
+
+    // move forward the given amount, with the pen down
+    public void goForward(double length) {
+        double oldx = x;
+        double oldy = y;
+        x += length * Math.cos(angle);
+        y += length * Math.sin(angle);
+        g.drawLine((int)oldx, (int)oldy, (int)x, (int)y);
     }
 }
