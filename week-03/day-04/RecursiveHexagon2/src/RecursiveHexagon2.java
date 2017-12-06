d6ced8d37b34efc6805507c2cfa16f1b845ee5fe
nguyenannie <tramtramrhinomo@gmail.com>
2017-12-06 11:35:41 +0100

    Fix the rounding problem with RecursiveHexagon2 exercise

--- week-03/day-04/RecursiveHexagon2/src/RecursiveHexagon2.java
+++ week-03/day-04/RecursiveHexagon2/src/RecursiveHexagon2.java
@@ -7,42 +7,42 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class RecursiveHexagon2 {
 
     public static void mainDraw(Graphics g) {
-        recursiveHexagon(g,WIDTH/2,WIDTH/2,300,5);
+        recursiveHexagon(g,WIDTH/2,WIDTH/2,200,4);
 
     }
-    public static void recursiveHexagon(Graphics g, int x, int y, int size, int n) {
+    public static void recursiveHexagon(Graphics g, double x, double y, int size, int n) {
         if(n == 0) {
             return;
         }
         drawHexagon(g,x,y,size);
-        int height = (int)(Math.sqrt(3.0) * size / 2);
+        double height = Math.sqrt(3.0) * size / 2;
 
-        int x0 = x - size/3;
-        int y0 = y -2 *height/3;
+        double x0 = x - size/3;
+        double y0 = y -2 *height/3;
 
-        int x1 = x - 2 * size/3;
-        int y1 = y;
+        double x1 = x - 2 * size/3;
+        double y1 = y;
 
-        int x2 = x - size/3;
-        int y2 = y + 2 * height/3;
+        double x2 = x - size/3;
+        double y2 = y + 2 * height/3;
 
-        int x3 = x + size/3;
-        int y3 = y + 2 * height/3;
+        double x3 = x + size/3;
+        double y3 = y + 2 * height/3;
 
-        int x4= x + 2 * size/3;
-        int y4 = y;
+        double x4= x + 2 * size/3;
+        double y4 = y;
 
-        int x5 = x + size/3;
-        int y5 = y - 2 * height/3;
+        double x5 = x + size/3;
+        double y5 = y - 2 * height/3;
 
-        int[] xpoints = {x0,x1,x2,x3,x4,x5};
-        int[] ypoints = {y0,y1,y2,y3,y4,y5};
+        int[] xpoints = {(int)x0,(int)x1,(int)x2,(int)x3,(int)x4,(int)x5};
+        int[] ypoints = {(int)y0,(int)y1,(int)y2,(int)y3,(int)y4,(int)y5};
 
-        for(int i = 0; i < xpoints.length; i++) {
+        /*for(int i = 0; i < xpoints.length; i++) {
             recursiveHexagon(g,xpoints[i],ypoints[i],size/3, n - 1);
         }
+        */
 
-        /*
 
         recursiveHexagon(g,x0,y0,size/3, n - 1);
         recursiveHexagon(g,x1,y1,size/3, n - 1);
@@ -50,30 +50,30 @@ public class RecursiveHexagon2 {
         recursiveHexagon(g,x3,y3,size/3, n - 1);
         recursiveHexagon(g,x4,y4,size/3, n - 1);
         recursiveHexagon(g,x5,y5,size/3, n - 1);
-        */
+        
     }
 
-    public static void drawHexagon(Graphics g, int x, int y, int size) {
-        int height = (int)(Math.sqrt(3.0) * size / 2);
-        int x0 = x - size/2;
-        int y0 = y - height;
+    public static void drawHexagon(Graphics g, double x, double y, int size) {
+        double height = (int)(Math.sqrt(3.0) * size / 2);
+        double x0 = x - size/2;
+        double y0 = y - height;
 
-        int x1 = x - size;
-        int y1 = y;
+        double x1 = x - size;
+        double y1 = y;
 
-        int x2 = x - size/2;
-        int y2 = y + height;
+        double x2 = x - size/2;
+        double y2 = y + height;
 
-        int x3 = x + size/2;
-        int y3 = y + height;
+        double x3 = x + size/2;
+        double y3 = y + height;
 
-        int x4 = x + size;
-        int y4 = y;
+        double x4 = x + size;
+        double y4 = y;
 
-        int x5 = x  + size/2;
-        int y5 = y - height;
+        double x5 = x  + size/2;
+        double y5 = y - height;
 
-        g.drawPolygon(new int[]{x0,x1,x2,x3,x4,x5}, new int[]{y0,y1,y2,y3,y4,y5},6);
+        g.drawPolygon(new int[]{(int)x0,(int)x1,(int)x2,(int)x3,(int)x4,(int)x5}, new int[]{(int)y0,(int)y1,(int)y2,(int)y3,(int)y4,(int)y5},6);
     }
 
 
