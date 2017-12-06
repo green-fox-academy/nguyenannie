60cb301a20187f74a7fbb1129fea725089152887
nguyenannie <tramtramrhinomo@gmail.com>
2017-12-06 16:20:36 +0100

    Complete Animal exercise

---
+++ week-04/day-01/Animal/src/Animal.java
@@ -0,0 +1,26 @@
+public class Animal {
+    int hunger = 50;
+    int thirst = 50;
+
+    public Animal(){
+        this(50,50);
+    }
+
+    public Animal(int hunger, int thirst){
+        this.hunger = hunger;
+        this.thirst = thirst;
+    }
+
+    public void eat() {
+        hunger -= 1;
+    }
+
+    public void drink() {
+        thirst -= 1;
+    }
+
+    public void play() {
+        hunger += 1;
+        thirst += 1;
+    }
+}
---
+++ week-04/day-01/Domino/src/Domino.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Domino {
+    List<int[]> dominos = new ArrayList<>();
+
+    public void setDominos(List<int[]> dominos) {
+        this.dominos = dominos;
+    }
+
+    public List<int[]> getDominos() {
+        return dominos;
+    }
+
+    public ArrayList<int[]> order(ArrayList<int[]> dominoes){
+
+    }
+}
