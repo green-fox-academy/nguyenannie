300f3e7366f25eead0adf98fa617fd1599b8c256
nguyenannie <tramtramrhinomo@gmail.com>
2017-12-06 16:27:27 +0100

    Complete Dice exercise

--- week-04/day-01/Dice/src/Dice.java
+++ week-04/day-01/Dice/src/Dice.java
@@ -3,22 +3,31 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Dice {
+    /*
     int dice1;
     int dice2;
     int dice3;
     int dice4;
     int dice5;
     int dice6;
+    */
 
-    List<Integer> diceList = new ArrayList<Integer>(Arrays.asList(this.dice1,this.dice2,dice3,dice4,dice5,dice6));
+    List<Integer> diceList = new ArrayList<Integer>();
 
     public void roll() {
         for(int i = 0; i < diceList.size(); i ++) {
-            this.diceList.get(i) =  (int) (Math.random()*6)+ 1;
+            //diceList.set(i, (int) (Math.random()*6)+ 1);
+            this.diceList.add(i,(int) (Math.random()*6)+ 1);
         }
     }
 
     public List<Integer> getCurrent() {
         return this.diceList;
+    }
+
+    public void reroll() {
+        for(int i = 0; i < diceList.size(); i ++) {
+            this.diceList.set(i, (int) (Math.random()*6)+ 1);
+        }
     }
 }
