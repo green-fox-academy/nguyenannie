8a19df37a541602ab01b0480523e85a8cc49f800
nguyenannie <tramtramrhinomo@gmail.com>
2017-12-11 14:45:25 +0100

    Create war function

--- week-04/day-01/Pirates/src/Armada.java
+++ week-04/day-01/Pirates/src/Armada.java
@@ -7,10 +7,27 @@ public class Armada {
     public Armada() {
         ships = new ArrayList<>();
     }
-    /*
+
     public boolean war(Armada anotherArmada) {
+        int index1 = 0;
+        int index2 = 0;
+        boolean result;
+        while(index1 < this.ships.size() && index2 < anotherArmada.ships.size()) {
+            for (int i = 0; i < this.ships.size(); i++) {
+                if (this.ships.get(index1).battle(anotherArmada.ships.get(index2))) {
+                    index2 += 1;
+                } else {
+                    index1 += 1;
 
+                }
+            }
+        }
+        if(this.ships.size() - index1 > anotherArmada.ships.size() - index2){
+            result = true;
+        } else {
+            result = false;
+        }
+        return result;
     }
-    */
-
 }
+
