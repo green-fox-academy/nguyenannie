54f6b1242083ddd4e3814e4e2563bdd1f87fab56
nguyenannie <tramtramrhinomo@gmail.com>
2017-12-11 15:38:13 +0100

    Complete Armada class and Apple test class

--- week-04/day-01/Pirates/src/Armada.java
+++ week-04/day-01/Pirates/src/Armada.java
@@ -12,16 +12,17 @@ public class Armada {
         int index1 = 0;
         int index2 = 0;
         boolean result;
-        while(index1 < this.ships.size() && index2 < anotherArmada.ships.size()) {
             for (int i = 0; i < this.ships.size(); i++) {
-                if (this.ships.get(index1).battle(anotherArmada.ships.get(index2))) {
-                    index2 += 1;
+                if(index1 < this.ships.size() && index2 < anotherArmada.ships.size()) {
+                    if (this.ships.get(index1).battle(anotherArmada.ships.get(index2))) {
+                        index2 += 1;
+                    } else {
+                        index1 += 1;
+                    }
                 } else {
-                    index1 += 1;
-
+                    break;
                 }
             }
-        }
         if(this.ships.size() - index1 > anotherArmada.ships.size() - index2){
             result = true;
         } else {
---
+++ week-04/day-03/Apple/tests/AppleTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppleTest {
+    @Test
+    public void getApple() throws Exception {
+        Apple apple = new Apple();
+        assertEquals("Apple",apple.getApple());
+    }
+
+
+}
