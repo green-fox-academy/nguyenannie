1f4fe9aea75216f557a2bc6da4b2624b40f713e4
nguyenannie <tramtramrhinomo@gmail.com>
2017-12-20 11:41:34 +0100

    Add ListenForKey class

--- week-05/JavaGUI/SwingMore/EvenCounter/src/EventCounter.java
+++ week-05/JavaGUI/SwingMore/EvenCounter/src/EventCounter.java
@@ -1,12 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class EventCounter extends JFrame{
     JPanel panel;
     JButton mouse_Event_Counter, key_Event_Counter, window_Event_Counter;
-    JTextField textField;
+    JTextField mouse_Event_Text, key_Event_Text, window_Event_Text;
     JTextArea textArea;
     int counter;
 
@@ -29,24 +28,38 @@ public class EventCounter extends JFrame{
         panel = new JPanel();
         add(panel);
 
-        textField = new JTextField(30);
-        panel.add(textField);
+        textArea = new JTextArea(15,30);
+        panel.add(textArea);
+
+        mouse_Event_Text = new JTextField(30);
+        panel.add(mouse_Event_Text);
 
         mouse_Event_Counter = new JButton("Mouse Event Counter");
-        ButtonListener buttonListener = new ButtonListener();
-        mouse_Event_Counter.addActionListener(buttonListener);
+        ListenForButton listenForButton = new ListenForButton();
+        mouse_Event_Counter.addActionListener(listenForButton);
         panel.add(mouse_Event_Counter);
+
+        key_Event_Text = new JTextField(30);
+        ListenForKey listenForKey = new ListenForKey();
+        key_Event_Text.addKeyListener(listenForKey);
+        panel.add(key_Event_Text);
 
     }
 
-    private class ButtonListener implements ActionListener {
+    private class ListenForButton implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             if(e.getSource() == mouse_Event_Counter) {
                 counter++;
-                textField.setText("You have clicked the mouse for " + counter + " time.");
+                mouse_Event_Text.setText("You have clicked the mouse for " + counter + " time.");
             }
         }
     }
 
+    private class ListenForKey extends KeyAdapter {
+        @Override
+        public void keyTyped(KeyEvent e) {
+            textArea.append("You hits " + e.getKeyChar() + " ");
+        }
+    }
 }
