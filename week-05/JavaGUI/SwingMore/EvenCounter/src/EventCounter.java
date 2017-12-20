c1c234b87f55b3a873b09f8675861d0ad752d68b
nguyenannie <tramtramrhinomo@gmail.com>
2017-12-20 13:35:06 +0100

    Add ListenForWindow class

--- week-05/JavaGUI/SwingMore/EvenCounter/src/EventCounter.java
+++ week-05/JavaGUI/SwingMore/EvenCounter/src/EventCounter.java
@@ -3,18 +3,19 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class EventCounter extends JFrame{
-    JPanel panel;
-    JButton mouse_Event_Counter, key_Event_Counter, window_Event_Counter;
-    JTextField mouse_Event_Text, key_Event_Text, window_Event_Text;
-    JTextArea textArea;
-    int counter;
+    private JPanel panel;
+    private JButton button;
+    private JLabel button_Event_Text, key_Event_Display_Text, key_Event_Counter_Text;
+    private JTextField textField;
+    private JTextArea window_Event_Text;
+    private int mouseCounter, keyCounter;
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new EventCounter());
     }
 
     EventCounter() {
-        setSize(500,500);
+        setSize(500,300);
         setVisible(true);
         setResizable(false);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -27,39 +28,106 @@ public class EventCounter extends JFrame{
 
         panel = new JPanel();
         add(panel);
+        panel.setLayout(new BoxLayout(panel,BoxLayout.PAGE_AXIS));
 
-        textArea = new JTextArea(15,30);
-        panel.add(textArea);
+        button_Event_Text = new JLabel();
+        panel.add(button_Event_Text);
+        panel.add(Box.createRigidArea(new Dimension(0, 15)));
 
-        mouse_Event_Text = new JTextField(30);
-        panel.add(mouse_Event_Text);
+        key_Event_Display_Text = new JLabel();
+        panel.add(key_Event_Display_Text);
+        panel.add(Box.createRigidArea(new Dimension(0, 15)));
 
-        mouse_Event_Counter = new JButton("Mouse Event Counter");
+        key_Event_Counter_Text = new JLabel();
+        panel.add(key_Event_Counter_Text);
+        panel.add(Box.createRigidArea(new Dimension(0, 15)));
+
+        window_Event_Text = new JTextArea();
+        panel.add(window_Event_Text);
+        window_Event_Text.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(Box.createRigidArea(new Dimension(0, 15)));
+
+        button = new JButton("Click your mouse here!!!");
         ListenForButton listenForButton = new ListenForButton();
-        mouse_Event_Counter.addActionListener(listenForButton);
-        panel.add(mouse_Event_Counter);
+        button.addActionListener(listenForButton);
+        panel.add(button);
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(Box.createRigidArea(new Dimension(0, 15)));
 
-        key_Event_Text = new JTextField(30);
-        ListenForKey listenForKey = new ListenForKey();
-        key_Event_Text.addKeyListener(listenForKey);
-        panel.add(key_Event_Text);
+        textField = new JTextField(30);
+        ListenForKeyDisplay listenForKeyDisplay = new ListenForKeyDisplay();
+        textField.addKeyListener(listenForKeyDisplay);
+        ListenForKeyCounter listenForKeyCounter = new ListenForKeyCounter();
+        textField.addKeyListener(listenForKeyCounter);
+        panel.add(textField);
+
+        ListenForWindow listenForWindow = new ListenForWindow();
+        this.addWindowListener(listenForWindow);
 
     }
 
     private class ListenForButton implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            if(e.getSource() == mouse_Event_Counter) {
-                counter++;
-                mouse_Event_Text.setText("You have clicked the mouse for " + counter + " time.");
+            if(e.getSource() == button) {
+                mouseCounter++;
+                button_Event_Text.setText("You have clicked me for " + mouseCounter + " time.");
             }
         }
     }
 
-    private class ListenForKey extends KeyAdapter {
+    private class ListenForKeyDisplay extends KeyAdapter {
         @Override
         public void keyTyped(KeyEvent e) {
-            textArea.append("You hits " + e.getKeyChar() + " ");
+            key_Event_Display_Text.setText("You typed " + e.getKeyChar());
+
+        }
+    }
+
+    private class ListenForKeyCounter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+            keyCounter++;
+            key_Event_Counter_Text.setText("You pressed " + keyCounter + " times.");
+
+        }
+    }
+
+    private class ListenForWindow implements WindowListener {
+
+        @Override
+        public void windowOpened(WindowEvent e) {
+            window_Event_Text.append("Window is active!");
+        }
+
+        @Override
+        public void windowClosing(WindowEvent e) {
+            //window_Event_Text.append();
+        }
+
+        @Override
+        public void windowClosed(WindowEvent e) {
+
+        }
+
+        @Override
+        public void windowIconified(WindowEvent e) {
+
+        }
+
+        @Override
+        public void windowDeiconified(WindowEvent e) {
+            window_Event_Text.append("Window is in Normal state.");
+        }
+
+        @Override
+        public void windowActivated(WindowEvent e) {
+            window_Event_Text.append("Window is active.");
+        }
+
+        @Override
+        public void windowDeactivated(WindowEvent e) {
+            window_Event_Text.append("Window is not active.");
         }
     }
 }
