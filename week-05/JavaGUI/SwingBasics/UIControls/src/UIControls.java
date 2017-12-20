d9b3fe588c41635aa765c468a17f7211cf1621c6
nguyenannie <tramtramrhinomo@gmail.com>
2017-12-20 09:36:41 +0100

    Add clearMemory function and reorganize the code

--- week-05/JavaGUI/SwingBasics/UIControls/src/UIControls.java
+++ week-05/JavaGUI/SwingBasics/UIControls/src/UIControls.java
@@ -1,12 +1,10 @@
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 class UIControls extends JFrame{
     private JTextField textField;
-    private JTextArea showLabel, memoryLabel;
+    private JTextArea showText, showMemory;
     private String memmory;
     private JButton clearMemory;
     private JPanel panel;
@@ -27,44 +25,68 @@ class UIControls extends JFrame{
         setResizable(false);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-
         display();
     }
 
     void display() {
+        setPanel();
+
+        textField = new JTextField(15);
+        textFieldAction();
+        textFieldDisplay();
+
+        showText = new JTextArea();
+        textDisplay(showText);
+
+        showMemory = new JTextArea();
+        textDisplay(showMemory);
+
+        clearButton();
+    }
+
+    private void setPanel() {
         panel = new JPanel();
         add(panel);
         panel.setBorder(new EmptyBorder(2, 3, 2, 3));
         panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
-
-        textField = new JTextField(15);
-        memmory = "";
-
-        textField.addActionListener(e -> {
-            String input = textField.getText();
-            showLabel.setText(input);
-            memmory += " " + input;
-            memoryLabel.setText(memmory.substring(0, memmory.length() - input.length()));
-            textField.setText("");
-
-        });
-
-        textField.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panel.add(textField);
-        panel.add(Box.createRigidArea(new Dimension(0, 20)));
-
-        showLabel = new JTextArea();
-        labelDisplay(showLabel);
-
-        memoryLabel = new JTextArea();
-        labelDisplay(memoryLabel);
-
     }
 
-    void labelDisplay(JTextArea textArea) {
+    private void textDisplay(JTextArea textArea) {
         textArea.setAlignmentX(Component.CENTER_ALIGNMENT);
         textArea.setLineWrap(true);
         panel.add(textArea);
         panel.add(Box.createRigidArea(new Dimension(0, 15)));
+    }
+
+    private void textFieldDisplay() {
+        textField.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(textField);
+        panel.add(Box.createRigidArea(new Dimension(0, 20)));
+    }
+
+    private void textFieldAction() {
+        textField.addActionListener(e -> {
+            String input = textField.getText();
+            showText.setText(input);
+            memmory += " " + input;
+            showMemory.setText(memmory.substring(0, memmory.length() - input.length()));
+            textField.setText("");
+
+        });
+    }
+
+    private void clearButton() {
+        clearMemory = new JButton();
+        clearMemory.setText("Clear");
+        clearMemory.setAlignmentX(Component.CENTER_ALIGNMENT);
+        clear();
+        panel.add(clearMemory);
+    }
+
+    private void clear() {
+        clearMemory.addActionListener(e -> {
+            memmory = "";
+            showMemory.setText(memmory);
+        });
     }
 }
