9b0e5cc2fde2d70c9b3d011fa460e79a33b29df9
nguyenannie <tramtramrhinomo@gmail.com>
2017-12-20 10:46:51 +0100

    Complete ImageApp

---
+++ week-05/JavaGUI/SwingBasics/ImageApp/src/ImageApp.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class ImageApp extends JFrame {
+    private Image image = new ImageIcon("floral-bunny.png").getImage();
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new ImageApp());
+    }
+
+    private ImageApp() {
+        setSize(600, 750);
+        setTitle("ImageApp");
+        setResizable(false);
+        setVisible(true);
+
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension dim = tk.getScreenSize();
+        int xPos = (dim.width - this.getWidth()) / 2;
+        int yPos = (dim.height - this.getHeight()) / 2;
+        setLocation(xPos, yPos);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                requestFocus();
+                g.drawImage(image, 50,30, null);
+            }
+        };
+        add(panel);
+
+    }
+
+
+}
