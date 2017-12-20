6b79b4910a1e37b40c2eabb84f6fc347568ac364
nguyenannie <tramtramrhinomo@gmail.com>
2017-12-20 14:00:08 +0100

    Complete EventCounter

--- week-05/JavaGUI/SwingMore/EvenCounter/src/EventCounter.java
+++ week-05/JavaGUI/SwingMore/EvenCounter/src/EventCounter.java
@@ -3,19 +3,16 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class EventCounter extends JFrame{
-    private JPanel panel;
     private JButton button;
-    private JLabel button_Event_Text, key_Event_Display_Text, key_Event_Counter_Text;
-    private JTextField textField;
-    private JTextArea window_Event_Text;
+    private JLabel button_Event_Text, key_Event_Display_Text, key_Event_Counter_Text, window_Event_Text, mouse_Event_Text1, mouse_Event_Text2, mouse_Event_Text3,mouse_Event_Text4;
     private int mouseCounter, keyCounter;
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new EventCounter());
     }
 
-    EventCounter() {
-        setSize(500,300);
+    private EventCounter() {
+        setSize(300,500);
         setVisible(true);
         setResizable(false);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -26,44 +23,61 @@ public class EventCounter extends JFrame{
         int yPos = (dim.height - getHeight()) / 2;
         setLocation(xPos,yPos);
 
-        panel = new JPanel();
+        JPanel panel = new JPanel();
         add(panel);
         panel.setLayout(new BoxLayout(panel,BoxLayout.PAGE_AXIS));
 
-        button_Event_Text = new JLabel();
+        button_Event_Text = new JLabel("Button event is displayed here :)");
         panel.add(button_Event_Text);
         panel.add(Box.createRigidArea(new Dimension(0, 15)));
 
-        key_Event_Display_Text = new JLabel();
+        key_Event_Display_Text = new JLabel("Here is key event.");
         panel.add(key_Event_Display_Text);
         panel.add(Box.createRigidArea(new Dimension(0, 15)));
 
-        key_Event_Counter_Text = new JLabel();
+        key_Event_Counter_Text = new JLabel("Gonna show you key counter.");
         panel.add(key_Event_Counter_Text);
         panel.add(Box.createRigidArea(new Dimension(0, 15)));
 
-        window_Event_Text = new JTextArea();
+        window_Event_Text = new JLabel("Window event.");
         panel.add(window_Event_Text);
-        window_Event_Text.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.add(Box.createRigidArea(new Dimension(0, 15)));
+
+        ListenForWindow listenForWindow = new ListenForWindow();
+        this.addWindowListener(listenForWindow);
+
+        mouse_Event_Text1 = new JLabel("Mouse Panel Event is displayed here");
+        panel.add(mouse_Event_Text1);
+        panel.add(Box.createRigidArea(new Dimension(0, 15)));
+
+        mouse_Event_Text2 = new JLabel("Mouse Screen Event is displayed here");
+        panel.add(mouse_Event_Text2);
+        panel.add(Box.createRigidArea(new Dimension(0, 15)));
+
+        mouse_Event_Text3 = new JLabel("Mouse Button Event is displayed here");
+        panel.add(mouse_Event_Text3);
+        panel.add(Box.createRigidArea(new Dimension(0, 15)));
+
+        mouse_Event_Text4 = new JLabel("Mouse Click Event is displayed here");
+        panel.add(mouse_Event_Text4);
+        panel.add(Box.createRigidArea(new Dimension(0, 15)));
+
+        ListenForMouse listenForMouse = new ListenForMouse();
+        panel.addMouseListener(listenForMouse);
 
         button = new JButton("Click your mouse here!!!");
         ListenForButton listenForButton = new ListenForButton();
         button.addActionListener(listenForButton);
         panel.add(button);
-        button.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.add(Box.createRigidArea(new Dimension(0, 15)));
 
-        textField = new JTextField(30);
+        JTextField textField = new JTextField(30);
         ListenForKeyDisplay listenForKeyDisplay = new ListenForKeyDisplay();
         textField.addKeyListener(listenForKeyDisplay);
         ListenForKeyCounter listenForKeyCounter = new ListenForKeyCounter();
         textField.addKeyListener(listenForKeyCounter);
         panel.add(textField);
-
-        ListenForWindow listenForWindow = new ListenForWindow();
-        this.addWindowListener(listenForWindow);
-
+        panel.add(Box.createRigidArea(new Dimension(0, 15)));
     }
 
     private class ListenForButton implements ActionListener {
@@ -97,7 +111,7 @@ public class EventCounter extends JFrame{
 
         @Override
         public void windowOpened(WindowEvent e) {
-            window_Event_Text.append("Window is active!");
+            window_Event_Text.setText("Window is active!");
         }
 
         @Override
@@ -117,17 +131,48 @@ public class EventCounter extends JFrame{
 
         @Override
         public void windowDeiconified(WindowEvent e) {
-            window_Event_Text.append("Window is in Normal state.");
+            //window_Event_Text.append("Window is in Normal state.");
         }
 
         @Override
         public void windowActivated(WindowEvent e) {
-            window_Event_Text.append("Window is active.");
+            //window_Event_Text.append("Window is active.");
         }
 
         @Override
         public void windowDeactivated(WindowEvent e) {
-            window_Event_Text.append("Window is not active.");
+            //window_Event_Text.append("Window is not active.");
+        }
+    }
+
+
+    private class ListenForMouse implements MouseListener{
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            mouse_Event_Text1.setText("Mouse Panel is at " + e.getX() + ", " + e.getY() + " coordinate.");
+            mouse_Event_Text2.setText("Mouse Screen is at " + e.getXOnScreen() + ", " + e.getYOnScreen() + " coordinate.");
+            mouse_Event_Text3.setText("Mouse Button is " + e.getButton());
+            mouse_Event_Text4.setText("Mouse is clicked " + e.getClickCount() + " times.");
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
         }
     }
 }
