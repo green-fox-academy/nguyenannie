74c27c4a742c21e3aaaa7b6e1ee34eb2b8638b05
nguyenannie <tramtramrhinomo@gmail.com>
2017-12-21 09:07:10 +0100

    Change boolean varibale to more descriptive name

--- week-05/pallida-exam-basics/UniqueChars/src/UniqueChars.java
+++ week-05/pallida-exam-basics/UniqueChars/src/UniqueChars.java
@@ -1,21 +1,21 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class UniqueChars {
+class UniqueChars {
 
-    public static List<String> findUniques(String str) {
+    static List<String> findUniques(String str) {
         List<String> result = new ArrayList<>();
 
         for(int i = 0; i < str.length(); i++) {
             char x = str.charAt(i);
-            boolean b = true;
+            boolean isUnique = true;
             for(int j = 0; j < str.length(); j++) {
                 if(x == str.charAt(j) && i != j) {
-                    b = false;
+                    isUnique = false;
                     break;
                 }
             }
-            if(b) {
+            if(isUnique) {
                 result.add(String.valueOf(x));
             }
         }
