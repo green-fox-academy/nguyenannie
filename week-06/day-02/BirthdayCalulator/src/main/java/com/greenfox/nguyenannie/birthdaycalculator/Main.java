97d2fb090e683038ad779023edc9eed21b72eebb
nguyenannie <tramtramrhinomo@gmail.com>
2017-12-26 23:48:47 +0100

    Complete birthday calculator exercise

---
+++ week-06/day-02/BirthdayCalulator/src/main/java/com/greenfox/nguyenannie/birthdaycalculator/Main.java
@@ -0,0 +1,19 @@
+package com.greenfox.nguyenannie.birthdaycalculator;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static final String MY_BIRTHDAY = "1995-05-27";
+
+    public static void main(String[] args) {
+        BirthdayCalculator<LocalDate> birthdayCalculator = new BirthdayWithLocalDate();
+        LocalDate myBirthDate = birthdayCalculator.parseDate(MY_BIRTHDAY);
+
+        System.out.println(args[0]);
+
+        System.out.println(birthdayCalculator.printMonthAndDay(myBirthDate));
+        System.out.println("Is today my birthday? " + birthdayCalculator.isAnniversaryToday(myBirthDate));
+        System.out.println("I am " + birthdayCalculator.calculateAgeInYears(myBirthDate) + " year old till this moment.");
+        System.out.println("There are still " + birthdayCalculator.calculateDaysToNextAnniversary(myBirthDate) + " days till my next birthday.");
+    }
+}
