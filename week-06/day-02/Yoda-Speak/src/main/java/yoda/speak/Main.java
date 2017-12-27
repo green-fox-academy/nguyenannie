fa8e686be47cf6a40ed5e6d90fe3fa3c5342f955
nguyenannie <tramtramrhinomo@gmail.com>
2017-12-27 22:25:42 +0100

    Complete yoda-speak exercise

---
+++ week-06/day-02/Yoda-Speak/src/main/java/yoda/speak/Main.java
@@ -0,0 +1,25 @@
+package yoda.speak;
+
+import com.mashape.unirest.http.exceptions.UnirestException;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, UnirestException {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://yoda.p.mashape.com/yoda/")
+                .build();
+        YoDaSpeak yoDaSpeak = retrofit.create(YoDaSpeak.class);
+
+        Call<ResponseBody> responseBodyCall =
+                yoDaSpeak.translate("Rub rub is a bad ket and sonic is the same.");
+
+        Response<ResponseBody> response = responseBodyCall.execute();
+
+        System.out.println(response.body().string());
+    }
+}
