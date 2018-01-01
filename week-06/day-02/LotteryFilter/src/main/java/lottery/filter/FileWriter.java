af362d909ed52976e455f09e203c1c4685a912bd
nguyenannie <tramtramrhinomo@gmail.com>
2018-01-01 15:11:37 +0100

    Handle exception in FileWriter than ducking it

--- week-06/day-02/LotteryFilter/src/main/java/lottery/filter/FileWriter.java
+++ week-06/day-02/LotteryFilter/src/main/java/lottery/filter/FileWriter.java
@@ -10,15 +10,23 @@ public class FileWriter {
     private Writer writer;
     private CSVWriter csvWriter;
 
-    public FileWriter(String filePath) throws IOException {
-        writer = new java.io.FileWriter(filePath);
+    public FileWriter(String filePath) {
+        try {
+            writer = new java.io.FileWriter(filePath);
+        } catch (IOException e) {
+            System.err.println("Error in print Stack Trace: " + e.getMessage());
+        }
         csvWriter =
                 new CSVWriter(writer, ';', CSVWriter.NO_QUOTE_CHARACTER,
                         CSVWriter.NO_ESCAPE_CHARACTER, "\n");
     }
 
-    public void writeFile(List<String[]> allLines) throws IOException {
+    public void writeFile(List<String[]> allLines){
         csvWriter.writeAll(allLines);
-        writer.close();
+        try {
+            writer.close();
+        } catch (IOException e) {
+            System.err.println("Error in print Stack Trace: " + e.getMessage());
+        }
     }
 }
