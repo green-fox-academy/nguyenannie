a86f5c879a8f9761bea37ea49a277c9a8829e945
nguyenannie <tramtramrhinomo@gmail.com>
2018-01-02 11:50:48 +0100

    Refactor code in exercise06

---
+++ week-06/day-02/Stream/Exercise06/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(findFrequencyOfSpecificChar("Rubrub is a bad cat and soni is the same", 'b'));
+        findFrequency("abaaabbbcc     dddc");
+    }
+
+    public static long findFrequencyOfSpecificChar(String str, Character c){
+        return Stream.of(str.split("[\\s\\t\\r\\n\\f:*+]*"))
+                .filter(s -> s.equals(c.toString()))
+                .count();
+    }
+
+    public static void findFrequency(String str) {
+        System.out.println(Stream.of(str.split("[\\s\\t\\r\\n\\f:*+]*"))
+                                .collect(Collectors.groupingBy(s -> s.toString(), Collectors.counting())));
+    }
+
+}
