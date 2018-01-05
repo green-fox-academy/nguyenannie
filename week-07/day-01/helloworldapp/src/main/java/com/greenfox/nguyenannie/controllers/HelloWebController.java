fb26d5bb73c8202969f45d1ddf50f3a81d07481a
nguyenannie <tramtramrhinomo@gmail.com>
2018-01-05 13:32:30 +0100

    add Web Greet counter app

--- week-07/day-01/helloworldapp/src/main/java/com/greenfox/nguyenannie/controllers/HelloWebController.java
+++ week-07/day-01/helloworldapp/src/main/java/com/greenfox/nguyenannie/controllers/HelloWebController.java
@@ -5,18 +5,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 @SpringBootApplication
 public class HelloWebController {
 
+    AtomicLong atomicLong = new AtomicLong(3);
+
     public static void main(String[] args) {
         SpringApplication.run(HelloRESTController.class, args);
     }
 
-    @RequestMapping("/web/greeting")
-    public String greeting(Model model) {
-        model.addAttribute("name", " World");
-        return "greeting";
+    @ResponseBody
+    @RequestMapping(value = "/web/greeting")
+    public Greeting greeting(@RequestParam(value = "name", required = false) String name, Model map) {
+        map.addAttribute("Your name is: " + name);
+        return new Greeting(atomicLong.addAndGet(1), "hello " + name);
     }
 }
