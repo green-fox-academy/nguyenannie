85dce25effa4ba9ded8fe8405313a6ee446da976
nguyenannie <tramtramrhinomo@gmail.com>
2018-01-25 17:29:18 +0100

    Add login authorization

--- week-08/projectdays/MyReddit/src/main/java/com/greenfoxacademy/reddit/Controller/LoginController.java
+++ week-08/projectdays/MyReddit/src/main/java/com/greenfoxacademy/reddit/Controller/LoginController.java
@@ -39,6 +39,15 @@ public class LoginController {
             result = "cannotdoit";
         }
 
+        try {
+            Object flash = request.getSession().getAttribute("flash");
+            model.addAttribute("flash", flash);
+
+            request.getSession().removeAttribute("flash");
+        } catch (Exception ex) {
+
+        }
+
         model.addAttribute("user", user);
         return result;
     }
